19d5bcc8c605c2567aec75e9a5d4ea918da33d2a
unknown <antmogbox@yahoo.com>
2018-05-12 15:33:29 +0300

    +refactoring +fixes

--- src/main/java/com/infosystem/springmvc/model/TariffOption.java
+++ src/main/java/com/infosystem/springmvc/model/TariffOption.java
@@ -47,8 +47,7 @@ public class TariffOption implements Serializable {
             joinColumns = {@JoinColumn(name = "OPTION_ID", nullable = false, referencedColumnName = "OPTION_ID")},
             inverseJoinColumns = {@JoinColumn(name = "EXCLUDING_OPTION_ID", nullable = false, referencedColumnName = "OPTION_ID")})
     private Set<TariffOption> excludingTariffOptions = new HashSet<TariffOption>();
-
-//
+    
 //    @Override
 //    public String toString() {
 //        return "TariffOption [id=" + id + ", name=" + name + ", price=" + price
@@ -63,26 +62,5 @@ public class TariffOption implements Serializable {
 //        result = prime * result + ((id == null) ? 0 : id.hashCode());
 //        return result;
 //    }
-//
-//    @Override
-//    public boolean equals(Object obj) {
-//        if (this == obj)
-//            return true;
-//        if (obj == null)
-//            return false;
-//        if (!(obj instanceof TariffOption))
-//            return false;
-//        TariffOption other = (TariffOption) obj;
-//        if (id == null) {
-//            if (other.id != null)
-//                return false;
-//        } else if (!id.equals(other.id))
-//            return false;
-//        if (id == null) {
-//            if (other.id != null)
-//                return false;
-//        } else if (!id.equals(other.id))
-//            return false;
-//        return true;
-//    }
+
 }
